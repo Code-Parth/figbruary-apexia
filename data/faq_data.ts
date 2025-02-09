5542cdfb092a440c5ab9647e16dd836db5eaa167
code-parth <softsparths@gmail.com>
2025-02-09 23:22:55 +0530

    ✅

---
+++ data/faq_data.ts
@@ -0,0 +1,38 @@
+const FAQData1 = [
+    {
+        value: "1",
+        question: "What is figbruary?",
+        answer: "Figbruary is a fictional month created by combining Figma + February where you can experiment and design things daily to push the limits.",
+    },
+    {
+        value: "2",
+        question: "Do I have to follow prompts strictly?",
+        answer: "No",
+    },
+    {
+        value: "3",
+        question: "If I don't understand the prompt?",
+        answer: "It's simple to understand, but if it becomes complex, you can reach out to X/Twitter or the community @apexia_club.",
+    }
+
+]
+
+const FAQData2 = [
+    {
+        value: "1",
+        question: "What is Apexia Hosting?",
+        answer: `Apexia is hosting a mini-version of figbruary. It will be the same concept as figbruary but, will go on for 7 Days.`,
+    },
+    {
+        value: "2",
+        question: "Do I have to post daily?",
+        answer: "No",
+    },
+    {
+        value: "3",
+        question: "Do I have to use Figma?",
+        answer: "No. Some prompts may be specifically for Figma, but most of them can be used with any graphic apps. You can also use either Sketch, Penpot, or maybe Illustrator. Give it a try; it's fun experimenting.",
+    },
+]
+
+export const FAQData = [FAQData1, FAQData2];
